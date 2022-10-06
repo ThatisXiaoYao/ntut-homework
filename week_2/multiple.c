59e51d1777809856058566585fab20ce05228e1d
BuildTools <unconfigured@null.spigotmc.org>
2022-10-06 22:01:23 +0800

    multiple

--- week_2/multiple.c
+++ week_2/multiple.c
@@ -1,10 +1,11 @@
-include <stdio.h>
+#include <stdio.h>
 
 int main(){
         int num1, num2;
         scanf("%d%d", &num1, &num2);
-        if(num2 / num1 == 0){
+        if(num1 % num2 == 0){
                 printf("the first is a multiple of the second");
         }
+        else
                 printf("the first is not a multiple of the second");
 }
