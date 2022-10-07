3a23ca5415e196cde333ca46517069c915848cef
XiaoYao <abc930206@gmail.com>
2022-10-07 14:46:31 +0800

    nothing

--- week_3/department_store.c
+++ week_3/department_store.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 
 int main(){
-	int account_ID, beginning_balance;
+	int account_ID, beginning_balance, total_charges;
 	printf("Enter account number (-1 to end): ");	
 	scanf("%d", &account_ID);
 	printf("%d\n", account_ID);	
 	printf("Enter your beginning balance: ");	
 	scanf("%d\n", &beginning_balance);
-	printf("%d\n", beginning_balance);	
+	printf("%d\n\n", beginning_balance);
+	printf("Enter total charges: ");	
+	scanf("%d", &total_charges);
+	printf("%d\n", total_charges);
 }
