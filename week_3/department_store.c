d586de139b9e73fb8592e428c80bc9f82bba2616
XiaoYao <abc930206@gmail.com>
2022-10-07 14:43:43 +0800

    department

---
+++ week_3/department_store.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int account_ID, beginning_balance;
+	printf("Enter account number (-1 to end): ");	
+	scanf("%d", &account_ID);
+	printf("%d\n", account_ID);	
+	printf("Enter your beginning balance: ");	
+	scanf("%d\n", &beginning_balance);
+	printf("%d\n", beginning_balance);	
+}
