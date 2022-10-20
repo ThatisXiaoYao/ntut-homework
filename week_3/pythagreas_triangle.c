0ce01b8334835614b05d9682cfde5adb9f1164dd
BuildTools <unconfigured@null.spigotmc.org>
2022-10-20 22:22:20 +0800

    lehrstoff

--- week_3/pythagreas_triangle.c
+++ week_3/pythagreas_triangle.c
@@ -6,7 +6,7 @@ int main()
     printf("150以內的勾股數有：\n");
     printf("  a    b    c      a    b    c      a    b    c      a    b    c\n");
     /*求150以內勾股數*/
-    for(a=1; a<=500; a++)
+    for(a=1; a<=500; a++){
         for(b=a+1; b<=500; b++)
         {
             c=(int)sqrt(a*a+b*b);  /*求c值*/
@@ -18,7 +18,7 @@ int main()
                     printf("\n");
             }
         }
- 
+    }
     printf("\n");
     return 0;
 }
