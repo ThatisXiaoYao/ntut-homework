2ba8faadfd6a80520688253acde57973196e52a1
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 23:24:49 +0800

    good

---
+++ week_5/double_power.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double Power(double, int);
+
+void main(void){
+        int k;
+        double Ans;
+        printf("calculate 3.5 k power? please input k=");
+        scanf("%d", &k);
+        Ans = Power(3.5, k); 
+        printf("calculate 3.5 %d power = %lf", k, Ans); 
+        system("pause");
+}
+double Power(double X, int n){
+        int i;
+        double PowerXn = 1;
+        for(i = 1; i <= n; i++){
+                PowerXn = PowerXn * X;
+        }
+        return PowerXn;
+}
--- week_5/fibonacci.c
+++ week_5/fibonacci.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 int main(){
-	int input, output[3];
-	scanf("%d", &input);
+	double input, output[3], i;
+	printf("function starting...\n");
+	scanf("%lf", &input);
 	output[0] = 0;
 	output[1] = 1;
-	printf("%d %d", output[0], output[1]);
+	printf("%.lf %.lf", output[0], output[1]);
 	for(i = 1; i <= input - 2; i++){
 		output[2] = output[1] + output[0];
-		printf("%d", output[2]);
+		printf(" %.lf", output[2]);
 		output[0] = output[1];
 		output[1] = output[2];
 	}
